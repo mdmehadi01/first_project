516c862917b1d090b74e027ce352b03ed287b614
md-mehadihassan <mdmehadihassan523@gmail.com>
2023-05-22 18:48:15 +0600

    sum,sub.mul& div

---
+++ sum,sub,div,mul/sum,sub.mul,div.php
@@ -0,0 +1,20 @@
+<?php
+$a=25;
+$b=35;
+$sum= ($a+$b);
+echo "The sum is: ".$sum;
+
+$a=25;
+$b=35;
+$sub= ($a-$b);
+echo "the sub is: ".$sub;
+
+$a=25;
+$b=5;
+$mul= ($a*$b);
+echo "the mul is: ".$mul;
+
+$a=25;
+$b=5;
+$div= ($a/$b);
+echo "the div is: ".$div;
