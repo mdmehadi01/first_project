d79e699695005c5817fdcc18eb4d39ee96c2059e
md-mehadihassan <mdmehadihassan523@gmail.com>
2023-05-27 17:47:50 +0600

    croud operation 2 page connection in php

---
+++ next33/list.php
@@ -0,0 +1,53 @@
+<?php
+    include('connection.php');
+
+    $query = "SELECT * FROM users";
+    $result = mysqli_query($connection,$query);
+
+    // while($data = mysqli_fetch_assoc($result)){
+    //     echo $data['name']. "<br>";
+    // }
+    
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Student List</title>
+</head>
+<body>
+
+        <a href="index.php">Home</a>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>First_Name</th>
+                    <th>Last_Name</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while($Row = mysqli_fetch_assoc($result)){ ?>
+                    <tr>
+                        <td><?php echo $Row['id']; ?></td>
+                        <td><?php echo $Row['First_Name']; ?></td>
+                        <td><?php echo $Row['Last_Name']; ?></td>
+                        <td><?php echo $Row['Email']; ?></td>
+                        <td><?php echo $Row['Password']; ?></td>
+                        <td><button>Edit</button></td>
+                        <td><button>Delete</button></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    
+</body>
+</html>
