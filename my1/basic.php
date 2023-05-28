0efeffabe525d5e8d0464b8120bab701f90c4b4d
md-mehadihassan <mdmehadihassan523@gmail.com>
2023-05-28 21:39:28 +0600

    php basic

---
+++ my1/basic.php
@@ -0,0 +1,12 @@
+<?php
+
+$one ="Hellow world!";//String
+$two =123;//Integer
+$three =12.5;//Float
+$foure =true;//booleam
+$five =array(123,"Mehadi");//Array
+$six = NULL;//NULL
+
+echo var_dump($five);
+
+?>
